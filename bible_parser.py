dcb3e8bd2fc6f172ff4fc7ed27c6efd85045e273
nnsviridov <nnsviridov@domclick.ru>
2020-01-31 02:30:39 +0300

    [upd] Парсер Библии

--- bible_parser.py
+++ bible_parser.py
@@ -1,7 +1,3 @@
 def bible_parser(start, end):
     with open(file='Bible.txt', mode='r', encoding='1251') as file:
-        final = ''
-        for x in range(10):
-            a = file.readline()
-            final += a
-        return final
+        return ''.join(file.readlines()[start:end])
