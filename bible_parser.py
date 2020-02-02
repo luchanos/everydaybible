f7590080da9136e0a07b81a09585cdbc8499ed0c
nnsviridov <nnsviridov@domclick.ru>
2020-02-02 18:25:14 +0300

    [upd] load bible func;

--- bible_parser.py
+++ bible_parser.py
@@ -3,11 +3,18 @@ from settings import STRING_LIMIT
 from utils import supress_errors
 
 
+def load_bible():
+    with open(file='Bible.txt', mode='r', encoding='1251') as bible:
+        return bible.readlines()
+
+
+loaded_bible = load_bible()
+
+
 @supress_errors
 def bible_parser(start=0, end=20):
     if end < start:
         raise WrongBorders("конечная строка не может быть меньше начальной!")
     if end - start > STRING_LIMIT:
         raise TooMuchLines("запрошен слишком большой промежуток строк! Лимит - {}!".format(STRING_LIMIT))
-    with open(file='Bible.txt', mode='r', encoding='1251') as file:
-        return ''.join(file.readlines()[start:end])
+    return ''.join(load_bible()[start:end])
