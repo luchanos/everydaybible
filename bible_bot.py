588d76095df69095723778dd2e3115f1156f535a
nnsviridov <nnsviridov@domclick.ru>
2020-04-19 20:08:35 +0300

    [upd] bot poller

--- bible_bot.py
+++ bible_bot.py
@@ -1,5 +1,9 @@
+import time
+
 import telebot
 from telebot import apihelper
+
+from image_worker import jesus_quote_image
 from main_commands import give_jesus, give_indulgence, bless_me, pray_coronavirus
 from settings import TOKEN, PROXY
 from utils import supress_errors
@@ -23,15 +27,43 @@ def command_router(message):
     commands[command_to_execute](message, bot)
 
 
+@bot.message_handler(commands=['test'])
+def jesus_quote(message):
+    bot.send_message(message.chat.id, reply_to_message_id=message.message_id, text='Введи текст цитаты, дитя моё')
+
+
+@bot.message_handler(commands=['next'])
 def waiting_for_message(message):
     bot.send_message(chat_id=message.chat.id, text='test')
 
 
+# словарь для хранения чуваков которые отправили цитату
+user_ids_for_quoting = set()
+
+
 @bot.message_handler(commands=['jesus_quote'])
 def jesus_quote(message):
+    """Бот принимает сообщение от пользователя и потом ожидает когда пользователь введёт цитату"""
+    user_ids_for_quoting.add(message.from_user.id)
     bot.send_message(message.chat.id, reply_to_message_id=message.message_id, text='Введи текст цитаты, дитя моё')
-    bot.register_next_step_handler(message=message, callback=waiting_for_message)
+
+
+@bot.message_handler(content_types=['text'])
+def quote_message_handler(message):
+    if message.from_user.id in user_ids_for_quoting:
+        # image = jesus_quote_image(top=message.text)
+        bot.send_message(message.chat.id, text='TEST')
+
+
+def telegram_polling():
+    try:
+        bot.polling(none_stop=True, timeout=60) #constantly get messages from Telegram
+    except Exception as err:
+        bot.stop_polling()
+        time.sleep(3)
+        telegram_polling()
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    telegram_polling()
+
--- main_commands.py
+++ main_commands.py
@@ -1,6 +1,4 @@
 import os
-from bible_parser import bible_parser
-from image_worker import jesus_quote_image
 from settings import PATH_TO_ICONS
 from utils import full_name_getter
 
@@ -14,22 +12,16 @@ def image_answer_sender(answer, img_name, bot, message):
 
 def bless_me(message, bot):
     answer = f"Благословляю тебя, {full_name_getter(message=message)}"
-    # bot.send_message(text='https://sun9-57.userapi.com/c855124/v855124788/1f8ee1/vdTFg3zC2KE.jpg' +
-    #                       ' ' + answer, chat_id=message.chat.id)
     image_answer_sender(answer=answer, img_name='blessing', bot=bot, message=message)
 
 
 def give_indulgence(message, bot):
     answer = f"Твои грехи отпущены, {full_name_getter(message=message)}"
-    # bot.send_message(text='https://sun9-35.userapi.com/c855124/v855124788/1f8eea/egynrCEJFjg.jpg' +
-    #                       ' ' + answer, chat_id=message.chat.id)
     image_answer_sender(answer=answer, img_name='sins', bot=bot, message=message)
 
 
 def give_jesus(message, bot):
     answer = f"Спаситель зрит на тебя, {full_name_getter(message=message)}"
-    # bot.send_message(text='https://sun9-43.userapi.com/c855124/v855124788/1f8ed2/XxhPpkG6Jzg.jpg' +
-    #                       ' ' + answer, chat_id=message.chat.id)
     image_answer_sender(answer=answer, img_name='есус', bot=bot, message=message)
 
 
@@ -43,14 +35,3 @@ def pray_coronavirus(message, bot):
              "Боже наш, и Тебе славу возсылаем, Отцу и Сыну и Святому Духу," \
              " ныне и присно и во веки веков. Аминь."
     image_answer_sender(answer=answer, img_name='coronavirus', bot=bot, message=message)
-
-
-def parse_args(s: str):
-    return s.split(' ')[1:]
-
-
-def give_bible_range(message, bot):
-    pic_url = 'https://www.pravmir.ru/wp-content/uploads/2017/08/bibliya-2.jpg'
-    args = list(map(int, parse_args(message.text)))
-    answer = bible_parser(start=args[0], end=args[1])
-    bot.send_message(text=pic_url + ' ' + answer, chat_id=message.chat.id)
